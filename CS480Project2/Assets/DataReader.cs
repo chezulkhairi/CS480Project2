a161ad2d1211c1c4c16186daa00a47dc1da89856
Jay Byam <jrbyam@alaska.edu>
2015-11-25 10:28:33 -0900

    The work from class on Monday

---
+++ CS480Project2/Assets/DataReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string text = System.IO.File.ReadAllText("Assets/test.txt");
+        string[] nums = text.Split (' ');
+        GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+        float x = 
+        float y =
+        float z =
+        cube.transform.position = new Vector3(x, y, z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
