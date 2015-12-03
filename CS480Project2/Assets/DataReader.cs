d97af4fe2337ecf33b13bc2f93112f991688f7fd
Jay Byam <jrbyam@alaska.edu>
2015-12-02 18:07:34 -0900

    Cleaned up DataReader.cs

--- CS480Project2/Assets/DataReader.cs
+++ CS480Project2/Assets/DataReader.cs
@@ -11,23 +11,7 @@ public class DataReader : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// Read in input
-//		SerialPort port = new SerialPort ("/dev/cu.usbmodemFA131", 9600);
-//		port.Open ();
-//		string text = port.ReadLine ().ToString ();
-//		print (text);
-//		string[] rawNums = text.Split (' ');
-//		// Convert input to floats
-//		float[] nums = new float[3];
-//		nums [0] = float.Parse(rawNums [0]);
-//		nums [1] = float.Parse(rawNums [1]);
-//		nums [2] = float.Parse(rawNums [2]);
-//		// Create and position cube representing object in space
-//		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-//		float x = nums [0] * Mathf.Cos (nums [1]);
-//		float y = nums [0] * -Mathf.Cos (nums [2]);
-//		float z = nums [0] * Mathf.Sin (nums [1]);
-//		cube.transform.position = new Vector3(x, y, z);
+
 	}
 
 	void readSerialPort () {
