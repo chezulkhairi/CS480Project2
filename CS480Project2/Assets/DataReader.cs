b26750de7d6db260cbc956a8a170f96757e8872f
Jay Byam <jrbyam@alaska.edu>
2015-12-02 18:04:36 -0900

    Succeeded in reading serial port from C# script in Unity

--- CS480Project2/Assets/DataReader.cs
+++ CS480Project2/Assets/DataReader.cs
@@ -1,28 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO.Ports;
 
 public class DataReader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		InvokeRepeating ("readSerialPort", 0, 0.1f);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		// Read in input
-		string text = System.IO.File.ReadAllText("Assets/test.txt");
+//		SerialPort port = new SerialPort ("/dev/cu.usbmodemFA131", 9600);
+//		port.Open ();
+//		string text = port.ReadLine ().ToString ();
+//		print (text);
+//		string[] rawNums = text.Split (' ');
+//		// Convert input to floats
+//		float[] nums = new float[3];
+//		nums [0] = float.Parse(rawNums [0]);
+//		nums [1] = float.Parse(rawNums [1]);
+//		nums [2] = float.Parse(rawNums [2]);
+//		// Create and position cube representing object in space
+//		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+//		float x = nums [0] * Mathf.Cos (nums [1]);
+//		float y = nums [0] * -Mathf.Cos (nums [2]);
+//		float z = nums [0] * Mathf.Sin (nums [1]);
+//		cube.transform.position = new Vector3(x, y, z);
+	}
+
+	void readSerialPort () {
+		SerialPort port = new SerialPort ("/dev/cu.usbmodemFA131", 9600);
+		port.Open ();
+		string text = port.ReadLine ().ToString ();
 		string[] rawNums = text.Split (' ');
-		// Convert input to floats
-		float[] nums = new float[3];
-		nums [0] = float.Parse(rawNums [0]);
-		nums [1] = float.Parse(rawNums [1]);
-		nums [2] = float.Parse(rawNums [2]);
-		// Create and position cube representing object in space
-		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		float x = nums [0] * Mathf.Cos (nums [1]);
-		float y = nums [0] * -Mathf.Cos (nums [2]);
-		float z = nums [0] * Mathf.Sin (nums [1]);
-		cube.transform.position = new Vector3(x, y, z);
+		if (rawNums.Length == 3) { // Avoid bad data
+			// Convert input to floats
+			float[] nums = new float[3];
+			nums [0] = float.Parse (rawNums [0]);
+			nums [1] = float.Parse (rawNums [1]);
+			nums [2] = float.Parse (rawNums [2]);
+			// Create and position cube representing object in space
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			float x = nums [0] * Mathf.Cos (nums [1]);
+			float y = 0; //nums [0] * -Mathf.Cos (nums [2]);
+			float z = nums [0] * Mathf.Sin (nums [1]);
+			cube.transform.position = new Vector3 (x, y, z);
+		}
 	}
 }
